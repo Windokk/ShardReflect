1247cf6953e1469e3142d30d27ff06615ccaaa3a
Windokk <68743762+Windokk@users.noreply.github.com>
2026-09-17 00:05:56 +0200

    Removed : run.sh/.bat inside build.bat/.sh

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 cmake -S . -B build -G "MinGW Makefiles"
 cd build
 cmake --build . -j8
-cd ..
-start run.bat
--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 cmake -S . -B build -G "Unix Makefiles"
 cd build
 cmake --build . -j8
-cd ..
-./run.sh
